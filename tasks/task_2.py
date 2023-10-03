18ddaed7006c59f325f0bb0c2a09151c0c39d325
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-10-03 13:30:51 +0100

    Проверка ДЗ | 6. Условные операторы
    - Правильно: №0, №2, №3,
    - Неправильно: №4
    - Улучшены: №0, №3,
    - На доработку: №1, №2, №3, №4,

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,11 +1,13 @@
-stocking_number: int = int(input('Введите целое число:'))
-if stocking_number > 0:
-    print('1')
-elif stocking_number < 0:
-    print('-1')
-else:
-    print('0')
+stocking_number: int = int(input('Введите целое число: '))
 
+if stocking_number > 0:
+    print(1)
+elif stocking_number < 0:
+    print(-1)
+else:
+    print(0)
+
+# -------------------- №2
 
 number: int = int(input('Введите целое число:'))
 condition = '1' if number > 0 else '-1'
--- tasks/task_4.py
+++ tasks/task_4.py
@@ -1,40 +1,34 @@
-number_1: int = int(input('Введите первое целое число:'))
-number_2: int = int(input('Введите второе целое число:'))
-number_3: int = int(input('Введите третье целое число:'))
+number_1: int = int(input('Введите первое целое число: '))
+number_2: int = int(input('Введите второе целое число: '))
+number_3: int = int(input('Введите третье целое число: '))
 
-if number_1 > number_2:
-    maximum = number_1
-    print('Наибольшее число:', maximum)
-else:
-    maximum = number_2
-    print('Наибольшее число:', maximum)
-if number_3 > maximum:
-    maximum = number_3
-    print('Наибольшее число:', maximum)
-else:
-    if number_1 == number_2 == number_3:
-        print('Все числа равны')
-
-
-# a = 2
-# b = 4
-# c = 9
-# if a > b:
-#     print(a)
-# elif a < b:
-#     print(b)
-# elif b > a:
-#     print(b)
-# elif b < a:
-#     print(a)
-# elif c > a:
-#     print(c)
-# elif c < a:
-#     print(a)
-# elif c > b:
-#     print(c)
-# elif c < b:
-#     print(b)
+# if number_1 > number_2:
+#     maximum = number_1
+#     print('Наибольшее число:', maximum)
 # else:
-#     print('=')
+#     maximum = number_2
+#     print('Наибольшее число:', maximum)
+# if number_3 > maximum:
+#     maximum = number_3
+#     print('Наибольшее число:', maximum)
+# else:
+#     if number_1 == number_2 == number_3:
+#         print('Все числа равны')
 
+"""
+Число_1
+Число_2
+Число_3
+
+Если Число_1 БОЛЬШЕ Число_2 И Число_1 БОЛЬШЕ Число_3
+    Наибольшее число Число_1
+
+Иначе если Число_2 БОЛЬШЕ Число_3
+    Наибольшее число Число_2
+
+Иначе если Число_3 БОЛЬШЕ Число_2
+    Наибольшее число Число_3
+
+Иначе
+    Все числа равны
+"""
