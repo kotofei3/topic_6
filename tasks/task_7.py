d52de7f92ba7c7302cac80c4447cd5954d8f8a72
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-10-05 13:28:03 +0100

    Проверка ДЗ | 6. Условные операторы
    - Правильно: №5,
    - Неправильно: №6, №7, №8, №9
    - Улучшены: №5, №6,
    - На доработку: №6, №7, №8, №9

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -1,14 +1,12 @@
-# number: int = int(input(":"))
-# number_2: float = float(input(":"))
-# number_3: str = input(":")
-# if type(number) == int:
-#     result = 'YES'
-#     if type(number_2) == float:
-#         result = 'YES'
-#         if type(number_3) == str:
-#             result = 'YES'
-#             print(result)
-# else:
-#     print('NO')
+number: int = int(input(":"))
+number_2: float = float(input(":"))
+number_3: str = input(":")
 
+# Подсказка к 7 и 9 задачке:
+# Тема №5
+# Встроенные функции Python: для работы с коллекциями и числами
 
+if ...:
+    print('Да')
+else:
+    print('Нет')
--- tasks/task_9.py
+++ tasks/task_9.py
@@ -1,11 +1,7 @@
-# number: int = int(input(":"))
-# number_2: float = float(input(":"))
-# line: str = input(":")
-# if type(number) != int:
-#     result = False
-# elif type(number_2) != float:
-#     result = False
-# elif type(line) != str:
-#     result = False
-# else:
-#     print(True)
+number: int = int(input(":"))
+number_2: float = float(input(":"))
+
+if ...:
+    ...
+else:
+    ...
